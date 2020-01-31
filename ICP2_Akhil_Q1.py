36042dc80d3a73bba87381ceb67186a8f9ce9581
akhilkanugolu <60271171+akhilkanugolu@users.noreply.github.com>
2020-01-31 13:07:27 -0600

    Add files via upload

---
+++ ICP2_Akhil_Q1.py
@@ -0,0 +1,14 @@
+no_of_students=int(input("Enter Number of Students:"))
+list_lb=[]
+list_kg=[]
+for i in range(no_of_students):
+        print("Enter Lb Weight of", i+1 ,"Student:")
+        lb_weight=int(input())
+        list_lb.append(lb_weight)
+        #list_kg.append(lb_weight*(0.453514))
+
+list_kg=[list_lb[i]*(0.453514) for i in range(no_of_students)]
+
+
+print("Weight of Students in lb",list_lb)
+print("Weight of Students in kg",list_kg)
